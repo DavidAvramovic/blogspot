edef08007ca34159bc5e4f45c1c41f0082602846
DavidAvramovic <david.avramovic@yahoo.com>
2018-01-24 15:04:18 +0100

    Users page working without photos

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
 class User extends Authenticatable
 {
     /**
@@ -12,7 +13,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','role_id','photo_id','is_active',
     ];
 
     /**
